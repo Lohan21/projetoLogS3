fe8eba9527c37cbc24845f31f7a79a74320c0f80
Lohan21 <lohan.muller.sampaio@gmail.com>
2024-01-19 00:44:18 -0300

    Progresso

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -3,7 +3,9 @@ sudo yum update -y
 sudo yum upgrade -y
 sudo yum install nodejs npm git nginx -y
 cd /home/ec2-user
-mkdir projeto
-sudo chmod 707 projeto
-cd projeto
+git clone https://github.com/Lohan21/projetoLogS3.git
+sudo chmod 707 projetoLogS3
+sudo chmod 707 projetoLogS3/src
+sudo cp projetoLogS3/config/*.conf /etc/nginx/
+cd projetoLogS3/src
 sudo npm install express request-ip aws-sdk
